d287c2c4a286f772685ff11b113cbf2a803c3e79
Marsyus <miadolomboy0917@gmail.com>
2025-04-07 14:35:46 +0800

    Created the necessary functions

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,11 @@
 # Quiz Creator
 #Create a function that asks user input to generate a question
+def make_question():
+    pass
 #Create a function that writes the question in a text file
+def make_file():
+    pass
 #Create a function that asks user to continue or not
+def ask_continue():
+    pass
 #Call the functions to run the program
