40f7ebce68ab25d0b318919ea14f158c04e9d779
Marsyus <miadolomboy0917@gmail.com>
2025-04-28 01:32:06 +0800

    Fixed indentations

--- quiz_creator.py
+++ quiz_creator.py
@@ -10,29 +10,29 @@ def make_question():
     d += input(f"Enter choice for letter d: ")
     choices = ["a", "b", "c", "d"]
     while True:
-	ans = input("Choose a correct answer [a/b/c/d]: ").lower()
-	if ans in choices:
-	    answer = "Answer: " + ans
-	    break
-	else:
-	    print("Invalid input. Please try again.")
+        ans = input("Choose a correct answer [a/b/c/d]: ").lower()
+        if ans in choices:
+            answer = "Answer: " + ans
+            break
+        else:
+            print("Invalid input. Please try again.")
     return question, a, b, c, d, answer
 #Create a function that writes the question in a text file
 def make_file(item):
     for text in item:
-	file = open("/storage/emulated/0/quiz.txt", "a")
-	file.write(f"{text}\n")
-	file.close()
+        file = open("/storage/emulated/0/quiz.txt", "a")
+        file.write(f"{text}\n")
+        file.close()
 #Create a function that asks user to continue or not
 def ask_continue():
     while True:
-	ans = input("Would you like to continue? [Y/N]: ").upper()
-	if ans == "Y":
-	    return True
-	elif ans == "N":
-	    return False
-	else:
-	    print("Invalid input. Please try again.")
+        ans = input("Would you like to continue? [Y/N]: ").upper()
+        if ans == "Y":
+            return True
+        elif ans == "N":
+            return False
+        else:
+            print("Invalid input. Please try again.")
 #Call the functions to run the program
 while True:
     quiz = make_question()
