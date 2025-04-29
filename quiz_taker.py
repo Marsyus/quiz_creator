9cb237d4ed3d2f54061ead7bcbd0425d79c28de9
Marsyus <miadolomboy0917@gmail.com>
2025-04-29 22:37:29 +0800

    Created a funtion to read files

--- quiz_taker.py
+++ quiz_taker.py
@@ -2,7 +2,10 @@
 #Import random
 
 #Create a function that reads a text file that contains questions generated from 'quiz_creator.py'
-
+def read_file(file_name):
+    with open(file_name, "r") as file:
+        contents = file.readlines()
+    
 #Create a function that randomizes the questions
 
 #Create a function that welcomes the user to start and how many items to quiz
