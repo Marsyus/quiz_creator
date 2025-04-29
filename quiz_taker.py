784d4a0dbca5cfa8b7924e8e7cfff8df8c5b7c58
Marsyus <miadolomboy0917@gmail.com>
2025-04-29 22:30:48 +0800

    Program pseudocode

--- quiz_taker.py
+++ quiz_taker.py
@@ -1,1 +1,10 @@
 # Quiz Taker
+#Import random
+
+#Create a function that reads a text file that contains questions generated from 'quiz_creator.py'
+
+#Create a function that randomizes the questions
+
+#Create a function that welcomes the user to start and how many items to quiz
+
+#Call the functions to run the program
